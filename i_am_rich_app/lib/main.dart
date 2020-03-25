483a284487f15c04d8a1df3e79da40891d341498
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2020-03-24 22:34:53 -0300

    Fim Aula 27 - Sessao 3

--- i_am_rich_app/lib/main.dart
+++ i_am_rich_app/lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text("I am Rich"),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-        ),
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title: Text("I am Rich"),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: Center(
+              child: Image(
+                image: NetworkImage(
+                    'https://www.petlove.com.br/images/breeds/197837/profile/original/shiba-p.jpg?1532540015'),
+              ),
+            )),
       ),
     );
