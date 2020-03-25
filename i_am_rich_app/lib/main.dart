ba09a2cf04ed9b729639132415dc96ab95f5fcf4
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2020-03-24 22:06:22 -0300

    Primeira Etapa - AppBar

---
+++ i_am_rich_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+        ),
+      ),
+    );
